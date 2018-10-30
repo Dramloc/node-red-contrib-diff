68d218c924a053cee60be051e9b3432213aafa28
Antoine Colmard <a.colmard@gmail.com>
2018-10-30 14:43:12 +0100

    :tada: Initial commit

---
+++ diff.js
@@ -0,0 +1,33 @@
+const get = require('lodash/get');
+
+module.exports = (RED) => {
+  function diff(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const { keyPath } = node.config;
+    node.lastPayload = null;
+
+    const diffPayloads = (previous, current) => {
+      current.reduce((result, item) => {
+        if (
+          previous.some(other => get(other, keyPath) === get(item, keyPath))
+        ) {
+          return [...result, item];
+        }
+        return result;
+      }, []);
+    };
+
+    node.on('input', (msg) => {
+      const { payload } = msg;
+      if (node.lastInput === null) {
+        node.lastPayload = payload;
+        return node.send({ payload });
+      }
+      const result = diffPayloads(node.lastPayload, payload);
+      node.lastPayload = payload;
+      return node.send({ payload: result });
+    });
+  }
+  RED.nodes.registerType('DIFF', diff);
+};
